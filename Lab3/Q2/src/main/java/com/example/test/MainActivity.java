f96e9eaa4b44a6ead9a117aa87e1266c136e02c2
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2025-02-04 07:37:59 +0530

    Add files via upload

---
+++ Lab3/Q2/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // List of sports
+        String[] sports = {"Football", "Basketball", "Cricket", "Tennis", "Badminton", "Hockey", "Volleyball"};
+
+        // Find ListView
+        ListView listView = findViewById(R.id.listView);
+
+        // Create Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sports);
+        listView.setAdapter(adapter);
+
+        // Handle Item Clicks
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedSport = sports[position];
+                Toast.makeText(MainActivity.this, "Selected: " + selectedSport, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
